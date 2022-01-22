aca52c1075c9f857025382936499bb9bcab6a9a1
simonihmig <simon.ihmig@kaliber5.de>
2022-01-22 13:49:51 +0100

    Migrate each(-in) rendering tests

---
+++ tests/unit/array-test.js
@@ -0,0 +1,149 @@
+import Component from '@glimmer/component';
+import { immutableTracked } from 'ember-immutable-tracked';
+
+import { setupRenderingTest } from 'ember-qunit';
+import { module } from 'qunit';
+import { reactivityTest } from '../helpers/reactivity';
+import {
+  eachReactivityTest,
+  eachInReactivityTest,
+} from '../helpers/collection-reactivity';
+
+const ARRAY_GETTER_METHODS = [
+  'concat',
+  'entries',
+  'every',
+  'filter',
+  'find',
+  'findIndex',
+  'flat',
+  'flatMap',
+  'forEach',
+  'includes',
+  'indexOf',
+  'join',
+  'keys',
+  'lastIndexOf',
+  'map',
+  'reduce',
+  'reduceRight',
+  'slice',
+  'some',
+  'values',
+];
+
+module('Array reactivity', function (hooks) {
+  setupRenderingTest(hooks);
+
+  eachReactivityTest(
+    '{{each}} works with new items',
+    class extends Component {
+      @immutableTracked
+      state = [0];
+
+      get collection() {
+        return this.state;
+      }
+
+      update() {
+        this.state = [0, 1];
+      }
+    }
+  );
+
+  eachReactivityTest(
+    '{{each}} works when updating old items',
+    class extends Component {
+      @immutableTracked
+      state = [0];
+
+      get collection() {
+        return this.state;
+      }
+
+      update() {
+        this.state = [1];
+      }
+    }
+  );
+
+  eachInReactivityTest(
+    '{{each-in}} works with new items',
+    class extends Component {
+      @immutableTracked
+      state = [0];
+
+      get collection() {
+        return this.state;
+      }
+
+      update() {
+        this.state = [0, 1];
+      }
+    }
+  );
+
+  eachInReactivityTest(
+    '{{each-in}} works when updating old items',
+    class extends Component {
+      @immutableTracked
+      state = [0];
+
+      get collection() {
+        return this.state;
+      }
+
+      update() {
+        this.state = [1];
+      }
+    }
+  );
+
+  ARRAY_GETTER_METHODS.forEach((method) => {
+    if (method !== 'keys') {
+      reactivityTest(
+        `${method} individual index`,
+        class extends Component {
+          @immutableTracked
+          state = [0];
+
+          get value() {
+            let value = this.state[method](() => {});
+
+            if (value && value.next) {
+              value.next();
+            }
+
+            return value;
+          }
+
+          update() {
+            this.state = [1];
+          }
+        }
+      );
+    }
+
+    reactivityTest(
+      `${method} collection`,
+      class extends Component {
+        @immutableTracked
+        state = [0];
+
+        get value() {
+          let value = this.state[method](() => {});
+
+          if (value && value.next) {
+            value.next();
+          }
+
+          return value;
+        }
+
+        update() {
+          this.state = [0, 1];
+        }
+      }
+    );
+  });
+});
---
+++ tests/unit/object-test.js
@@ -0,0 +1,99 @@
+import Component from '@glimmer/component';
+import { immutableTracked } from 'ember-immutable-tracked';
+
+import { setupRenderingTest } from 'ember-qunit';
+import { module } from 'qunit';
+import { reactivityTest } from '../helpers/reactivity';
+import { eachInReactivityTest } from '../helpers/collection-reactivity';
+
+module('Object reactivity', function (hooks) {
+  setupRenderingTest(hooks);
+
+  eachInReactivityTest(
+    '{{each-in}} works with new items',
+    class extends Component {
+      @immutableTracked
+      state = { 0: 0 };
+
+      get collection() {
+        return this.state;
+      }
+
+      update() {
+        this.state = { ...this.state, 1: 0 };
+      }
+    }
+  );
+
+  eachInReactivityTest(
+    '{{each-in}} works when updating old items',
+    class extends Component {
+      @immutableTracked
+      state = { foo: 0 };
+
+      get collection() {
+        return this.state;
+      }
+
+      update() {
+        this.state = { foo: 1 };
+      }
+    }
+  );
+
+  reactivityTest(
+    'individual index',
+    class extends Component {
+      @immutableTracked
+      state = { foo: 0 };
+
+      get value() {
+        return this.state.foo + 1;
+      }
+
+      update() {
+        this.state = { foo: 1 };
+      }
+    }
+  );
+
+  reactivityTest(
+    'iterating using for-in',
+    class extends Component {
+      @immutableTracked
+      state = { 1: 0 };
+
+      get value() {
+        let state = this.state;
+
+        let value = 0;
+
+        for (let foo in state) {
+          value = state[foo];
+        }
+
+        return value;
+      }
+
+      update() {
+        this.state = { ...this.state, 2: 0 };
+      }
+    }
+  );
+
+  reactivityTest(
+    'Object.keys',
+    class extends Component {
+      @immutableTracked
+      state = { 1: 0 };
+
+      get value() {
+        return Object.keys(this.state).join();
+      }
+
+      update() {
+        this.state = { ...this.state, 2: 0 };
+      }
+    }
+  );
+});
