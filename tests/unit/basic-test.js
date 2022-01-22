28246795dae73e2e06453080dbd1585a6dcc58b5
simonihmig <simon.ihmig@kaliber5.de>
2022-01-22 14:08:58 +0100

    Fix array iterator

--- tests/unit/basic-test.js
+++ tests/unit/basic-test.js
@@ -37,198 +37,281 @@ module('Unit | basic', () => {
   });
 
   module('object root', () => {
-    test('object updates correctly, does not invalidate cache', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = { static: 123, count: 1 };
-      }
+    module('object behavior', function () {
+      test('is an array', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = { foo: 0 };
+        }
 
-      const instance = new TestClass();
-      const cache = createCache(() => instance.value);
+        const instance = new TestClass();
 
-      assert.deepEqual(
-        getValue(cache),
-        { static: 123, count: 1 },
-        'initial value is correct'
-      );
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.strictEqual(typeof instance.value, 'object');
+        assert.true(instance.value instanceof Object);
+      });
 
-      instance.value = { static: 123, count: 2 };
+      test('is iterable', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = { foo: 0, bar: 1 };
+        }
 
-      assert.deepEqual(
-        getValue(cache),
-        { static: 123, count: 2 },
-        'initial value is correct'
-      );
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        const instance = new TestClass();
+
+        assert.deepEqual({ ...instance.value }, { foo: 0, bar: 1 });
+      });
+
+      test('supports Object methods', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = { foo: 0, bar: 1 };
+        }
+
+        const instance = new TestClass();
+        assert.deepEqual(Object.keys(instance.value), ['foo', 'bar']);
+        assert.deepEqual(Object.entries(instance.value), [
+          ['foo', 0],
+          ['bar', 1],
+        ]);
+      });
     });
 
-    test('cache updates correctly if dynamic value used', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = { static: 123, count: 1 };
-      }
+    module('reactivity', function () {
+      test('object updates correctly, does not invalidate cache', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = { static: 123, count: 1 };
+        }
 
-      const instance = new TestClass();
-      let cache = createCache(() => instance.value.count);
+        const instance = new TestClass();
+        const cache = createCache(() => instance.value);
 
-      assert.deepEqual(getValue(cache), 1, 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.deepEqual(
+          getValue(cache),
+          { static: 123, count: 1 },
+          'initial value is correct'
+        );
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
 
-      instance.value = { static: 123, count: 2 };
+        instance.value = { static: 123, count: 2 };
 
-      assert.deepEqual(getValue(cache), 2, 'initial value is correct');
-      assert.strictEqual(callCount(cache), 2, 'cache call count correct');
-    });
+        assert.deepEqual(
+          getValue(cache),
+          { static: 123, count: 2 },
+          'initial value is correct'
+        );
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+      });
 
-    test('cache not invalidated if static value used', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = { static: 123, count: 1 };
-      }
+      test('cache updates correctly if dynamic value used', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = { static: 123, count: 1 };
+        }
 
-      const instance = new TestClass();
-      let cache = createCache(() => instance.value.static);
+        const instance = new TestClass();
+        let cache = createCache(() => instance.value.count);
 
-      assert.deepEqual(getValue(cache), 123, 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.deepEqual(getValue(cache), 1, 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
 
-      instance.value = { static: 123, count: 2 };
+        instance.value = { static: 123, count: 2 };
 
-      assert.deepEqual(getValue(cache), 123, 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
-    });
+        assert.deepEqual(getValue(cache), 2, 'initial value is correct');
+        assert.strictEqual(callCount(cache), 2, 'cache call count correct');
+      });
 
-    test('cache invalidated correctly if collection used and keys changed', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = { foo: 1 };
-      }
+      test('cache not invalidated if static value used', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = { static: 123, count: 1 };
+        }
 
-      const instance = new TestClass();
-      let cache = createCache(() => Object.keys(instance.value));
+        const instance = new TestClass();
+        let cache = createCache(() => instance.value.static);
 
-      assert.deepEqual(getValue(cache), ['foo'], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.deepEqual(getValue(cache), 123, 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
 
-      instance.value = { foo: 1, bar: 1 };
+        instance.value = { static: 123, count: 2 };
 
-      assert.deepEqual(
-        getValue(cache),
-        ['foo', 'bar'],
-        'initial value is correct'
-      );
-      assert.strictEqual(callCount(cache), 2, 'cache call count correct');
-    });
+        assert.deepEqual(getValue(cache), 123, 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+      });
 
-    test('cache invalidated correctly if collection used and keys changed, same number of keys', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = { foo: 1 };
-      }
+      test('cache invalidated correctly if collection used and keys changed', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = { foo: 1 };
+        }
 
-      const instance = new TestClass();
-      let cache = createCache(() => Object.keys(instance.value));
+        const instance = new TestClass();
+        let cache = createCache(() => Object.keys(instance.value));
 
-      assert.deepEqual(getValue(cache), ['foo'], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.deepEqual(getValue(cache), ['foo'], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
 
-      instance.value = { bar: 1 };
+        instance.value = { foo: 1, bar: 1 };
 
-      assert.deepEqual(getValue(cache), ['bar'], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 2, 'cache call count correct');
+        assert.deepEqual(
+          getValue(cache),
+          ['foo', 'bar'],
+          'initial value is correct'
+        );
+        assert.strictEqual(callCount(cache), 2, 'cache call count correct');
+      });
+
+      test('cache invalidated correctly if collection used and keys changed, same number of keys', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = { foo: 1 };
+        }
+
+        const instance = new TestClass();
+        let cache = createCache(() => Object.keys(instance.value));
+
+        assert.deepEqual(getValue(cache), ['foo'], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+
+        instance.value = { bar: 1 };
+
+        assert.deepEqual(getValue(cache), ['bar'], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 2, 'cache call count correct');
+      });
     });
   });
 
   module('array root', () => {
-    test('array updates correctly, does not invalidate cache', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = [1];
-      }
+    module('array behavior', function () {
+      test('is an array', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = [1];
+        }
 
-      const instance = new TestClass();
-      const cache = createCache(() => instance.value);
+        const instance = new TestClass();
 
-      assert.deepEqual(getValue(cache), [1], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.true(Array.isArray(instance.value));
+        assert.true(instance.value instanceof Array);
+      });
 
-      instance.value = [1, 2];
+      test('is iterable', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = [1, 2];
+        }
 
-      assert.deepEqual(getValue(cache), [1, 2], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        const instance = new TestClass();
+
+        assert.deepEqual([...instance.value], [1, 2]);
+      });
+
+      test('supports Array methods', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = [1, 2];
+        }
+
+        const instance = new TestClass();
+
+        assert.deepEqual(
+          instance.value.map((i) => i + 1),
+          [2, 3]
+        );
+        assert.strictEqual(instance.value.length, 2);
+      });
     });
 
-    test('cache updates correctly if dynamic value used', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = [1];
-      }
+    module('reactivity', function () {
+      test('array updates correctly, does not invalidate cache', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = [1];
+        }
 
-      const instance = new TestClass();
-      let cache = createCache(() => instance.value[0]);
+        const instance = new TestClass();
+        const cache = createCache(() => instance.value);
 
-      assert.deepEqual(getValue(cache), 1, 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.deepEqual(getValue(cache), [1], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
 
-      instance.value = [2];
+        instance.value = [1, 2];
 
-      assert.deepEqual(getValue(cache), 2, 'initial value is correct');
-      assert.strictEqual(callCount(cache), 2, 'cache call count correct');
-    });
+        assert.deepEqual(getValue(cache), [1, 2], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+      });
 
-    test('cache not invalidated if static value used', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = [123, 1];
-      }
+      test('cache updates correctly if dynamic value used', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = [1];
+        }
 
-      const instance = new TestClass();
-      let cache = createCache(() => instance.value[0]);
+        const instance = new TestClass();
+        let cache = createCache(() => instance.value[0]);
 
-      assert.deepEqual(getValue(cache), 123, 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.deepEqual(getValue(cache), 1, 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
 
-      instance.value = [123, 2];
+        instance.value = [2];
 
-      assert.deepEqual(getValue(cache), 123, 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
-    });
+        assert.deepEqual(getValue(cache), 2, 'initial value is correct');
+        assert.strictEqual(callCount(cache), 2, 'cache call count correct');
+      });
 
-    test('cache invalidated correctly if iteration used and items added', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = [1];
-      }
+      test('cache not invalidated if static value used', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = [123, 1];
+        }
 
-      const instance = new TestClass();
-      let cache = createCache(() => instance.value.map((v) => v + 1));
+        const instance = new TestClass();
+        let cache = createCache(() => instance.value[0]);
 
-      assert.deepEqual(getValue(cache), [2], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.deepEqual(getValue(cache), 123, 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
 
-      instance.value = [1, 2];
+        instance.value = [123, 2];
 
-      assert.deepEqual(getValue(cache), [2, 3], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 2, 'cache call count correct');
-    });
+        assert.deepEqual(getValue(cache), 123, 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+      });
 
-    test('cache invalidated correctly if iteration used and items changed', function (assert) {
-      class TestClass {
-        @immutableTracked
-        value = [1];
-      }
+      test('cache invalidated correctly if iteration used and items added', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = [1];
+        }
 
-      const instance = new TestClass();
-      let cache = createCache(() => instance.value.map((v) => v + 1));
+        const instance = new TestClass();
+        let cache = createCache(() => instance.value.map((v) => v + 1));
 
-      assert.deepEqual(getValue(cache), [2], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+        assert.deepEqual(getValue(cache), [2], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
 
-      instance.value = [2];
+        instance.value = [1, 2];
 
-      assert.deepEqual(getValue(cache), [3], 'initial value is correct');
-      assert.strictEqual(callCount(cache), 2, 'cache call count correct');
+        assert.deepEqual(getValue(cache), [2, 3], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 2, 'cache call count correct');
+      });
+
+      test('cache invalidated correctly if iteration used and items changed', function (assert) {
+        class TestClass {
+          @immutableTracked
+          value = [1];
+        }
+
+        const instance = new TestClass();
+        let cache = createCache(() => instance.value.map((v) => v + 1));
+
+        assert.deepEqual(getValue(cache), [2], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 1, 'cache call count correct');
+
+        instance.value = [2];
+
+        assert.deepEqual(getValue(cache), [3], 'initial value is correct');
+        assert.strictEqual(callCount(cache), 2, 'cache call count correct');
+      });
     });
   });
 
