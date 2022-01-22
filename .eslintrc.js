e37864d2861b926d8567794d62305a9e4a0b2af2
simonihmig <simon.ihmig@kaliber5.de>
2022-01-22 11:46:44 +0100

    Setup TS & ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,24 @@ module.exports = {
   },
   rules: {},
   overrides: [
+    // ts files
+    {
+      files: ['**/*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-empty-function': 'warn',
+        '@typescript-eslint/explicit-module-boundary-types': 'error', // We want to be strict with types
+        '@typescript-eslint/explicit-function-return-type': 'error', // We want to be strict with types
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_' },
+        ],
+      },
+    },
     // node files
     {
       files: [
